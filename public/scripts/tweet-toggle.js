c9b6b7ab7a43f79222d40c4147254c97ef573c73
hildakh <h_khajehamiri@yahoo.com>
2019-11-08 17:15:34 +0000

    slideToggle was added to replace toggle for a more smooth experience. It is commented for now to be explored later

--- public/scripts/tweet-toggle.js
+++ public/scripts/tweet-toggle.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
   $('#arrow-button').click(function () {
     $('.new-tweet').toggle();
+    //ask about slideToggle later
+    // $('.new-tweet').slideToggle(800);
   });
+
 });

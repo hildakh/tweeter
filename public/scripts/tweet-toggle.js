c129142fe0a8795998ca601a3aa2e24e1d44a6f1
hildakh <h_khajehamiri@yahoo.com>
2019-11-08 23:32:16 +0000

    added a jquery focus function to make the tweet-box type ready after each toggle

--- public/scripts/tweet-toggle.js
+++ public/scripts/tweet-toggle.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
   $('#arrow-button').click(function () {
     $('.new-tweet').toggle();
+    $('#tweet-box').focus();
     //ask about slideToggle later
     // $('.new-tweet').slideToggle(800);
   });

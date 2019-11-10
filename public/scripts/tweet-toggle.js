16986a0abbd69254dfa2e868057fa5057085cfb0
hildakh <h_khajehamiri@yahoo.com>
2019-11-10 04:44:29 +0000

    removed some comments and added some essential ones

--- public/scripts/tweet-toggle.js
+++ public/scripts/tweet-toggle.js
@@ -1,11 +1,8 @@
-$(document).ready(function () {
-  $('#arrow-button').click(function () {
-    $('.new-tweet').toggle();
-    $('#tweet-box').focus();
+$(document).ready(function() {
+  $("#arrow-button").click(function() {
+    //toggle the compose box down on the click of the arrow button
+    $(".new-tweet").toggle();
+    //makes the text-area type area as soon as the compose box appears
+    $("#tweet-box").focus();
   });
-
-  //fix the time stamp using timeago plugin from jquery
-  // $('.time-stamp').text(timeago());
-
-
 });

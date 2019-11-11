9772b21b715b12bc2b724a72a90855a1d0249d7b
hildakh <h_khajehamiri@yahoo.com>
2019-11-11 05:24:07 +0000

    removed commented out code as per evaluator's advice

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,26 +1,19 @@
-$(document).ready(function () {
+$(document).ready(function() {
   //checking that the page loads before the alert is read
-  // alert('quelque chose de bien');
-  // --- our code goes here ---
-  let $counterBtn = $('#counter-btn');
+  let $counterBtn = $("#counter-btn");
   // find an element by id
-  const $tweetBox = $('#tweet-box');
+  const $tweetBox = $("#tweet-box");
   // assign it to a variable
   // add an event listener to it
   // log the event
-  $tweetBox.on('input', function () {
-    // console.log('keyup', $(this).val().length);
+  $tweetBox.on("input", function() {
     // set a variable to record the number of characters
     const $charCount = $(this).val().length;
     $counterBtn.text(140 - $charCount);
     if ($counterBtn.text() <= 0) {
-      $counterBtn.addClass('red');
+      $counterBtn.addClass("red");
     } else if ($counterBtn.text() > 0) {
-      $counterBtn.removeClass('red');
+      $counterBtn.removeClass("red");
     }
   });
-
-
-
 });
-
